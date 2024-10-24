8c2134d2788f9e7b06bf4a890c5cee2521d480ac
MelquesPaiva <melque1703@gmail.com>
2024-10-23 23:05:59 -0300

    First pages for project

---
+++ FirstProject/tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        system: {
+          primary: '#f01945',
+          medium: '#f36444',
+          light: '#ffce6f',
+          lighter: '#ffe3c9'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
